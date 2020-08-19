8a8f7aeccf0da707df782b27f1d6d656f568a369
Glorison <lai.glorison@gmail.com>
2020-08-20 03:27:37 +1000

    lint all the things

--- src/app/background/background.tsx
+++ src/app/background/background.tsx
@@ -1,15 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import bg from '../../assets/jay-manTREE.jpg';
 
 const Background = () => {
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    return ( 
-        loading ? 
-        <div className="background" />
-        :
-        <div className="not-background" />
-    )
-}
+  return loading ? <div className="background" /> : <div className="not-background" />;
+};
 
 export default Background;
