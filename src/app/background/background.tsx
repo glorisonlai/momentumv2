6be584b17676d2a753e96580bdeb55da21a21c07
Glorison <lai.glorison@gmail.com>
2020-08-20 03:21:45 +1000

    lint everything
    https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project

---
+++ src/app/background/background.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import bg from '../../assets/jay-manTREE.jpg';
+
+const Background = () => {
+    const [loading, setLoading] = useState(false);
+
+    return ( 
+        loading ? 
+        <div className="background" />
+        :
+        <div className="not-background" />
+    )
+}
+
+export default Background;
