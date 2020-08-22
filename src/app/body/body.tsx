31b8ae88c5685f825ddbf34cd213484ebb3c5108
Glorison <lai.glorison@gmail.com>
2020-08-22 16:32:56 +1000

    body

---
+++ src/app/body/body.tsx
@@ -0,0 +1,8 @@
+const React = require('react');
+
+const Body = () => {
+    return (
+        <div clasName="body">
+        
+    )
+}
---
+++ src/app/constants.ts
@@ -0,0 +1,26 @@
+const Constants: Object = {
+    const month: Array = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Sept',
+        'Oct',
+        'Nov',
+        'Dec',
+    ],
+    const day: Array = [
+        'Mon',
+        'Tue',
+        'Wed',
+        'Thur',
+        'Fri',
+        'Sat',
+        'Sun',
+    ]
+};
+
+module.exports = Constants;
