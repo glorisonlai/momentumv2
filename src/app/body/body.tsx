d14e94fe2773ec96d8465f1029d8e80e3df88de1
Glorison <lai.glorison@gmail.com>
2020-09-12 17:14:02 +1000

    move to other comp
    set up lambda
    set up dynamo

--- src/app/body/body.tsx
+++ src/app/body/body.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { months, days } from '../constants';
 import './body.css';
+import Weather from '../weather/weather';
 import emoji from 'emoji-dictionary';
 
 const Body: React.FunctionComponent = () => {
@@ -13,10 +14,11 @@ const Body: React.FunctionComponent = () => {
     hour = currentTime.getHours(),
     minute = currentTime.getMinutes();
 
-  useEffect(() => {
-    const refresh = async (timer: number) => {
-      await timer;
-      setTime(Date.now);
+  useEffect((): void => {
+    const refresh = (timer: number): void => {
+      setTimeout(function () {
+        setTime(Date.now);
+      }, timer);
     };
     const nextMin = Date.parse(
       `${year}-${formatDate(month + 1)}-${formatDate(date)}T${formatDate(hour)}:${formatDate(minute + 1)}:00`,
@@ -30,25 +32,21 @@ const Body: React.FunctionComponent = () => {
     return `00${dateElementStr}`.substring(dateElementStr.length);
   };
 
-  const Clock = () => {
-    return (
-      <h1>
-        <strong>
-          {formatDate(hour)}:{formatDate(minute)}
-        </strong>
-      </h1>
-    );
-  };
+  const Clock = (): React.FunctionComponentElement<void> => (
+    <h1>
+      <strong>
+        {formatDate(hour)}:{formatDate(minute)}
+      </strong>
+    </h1>
+  );
 
-  const CalendarDate = () => {
-    return (
-      <h1>
-        <strong>
-          {days[day]}, {months[month]} {date}
-        </strong>
-      </h1>
-    );
-  };
+  const CalendarDate = (): React.FunctionComponentElement<void> => (
+    <h1>
+      <strong>
+        {days[day]}, {months[month]} {date}
+      </strong>
+    </h1>
+  );
 
   return (
     <div className="body">
@@ -64,7 +62,7 @@ const Body: React.FunctionComponent = () => {
         <div className="favorite-links" />
       </div>
       <div className="right">
-        <div className="weather" />
+        <Weather />
       </div>
     </div>
   );
--- src/app/weather/weather.tsx
+++ src/app/weather/weather.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Weather: React.FunctionComponent = () => {
-  return <div className="weather" />;
+  const [expanded, setExpanded] = useState;
+  const temp = 14;
+  const suburb = 'Knox';
+  const convertToF = (temp: number): number => Math.round((temp * 9) / 5 + 32);
+  const tempString = (temp: number): string => {
+    let localMetric = window.localStorage.getItem('units');
+    if (!localMetric || !(localMetric in ['C', 'F'])) {
+      window.localStorage.setItem('units', 'C');
+      localMetric = 'C';
+    }
+
+    if (localMetric == 'C') return `${temp}`;
+    return `${convertToF(temp)}`;
+  };
+  return (
+    <div className="tile">
+      <div className="weather">
+        <span className="img" />
+        {tempString(temp)}&deg;
+      </div>
+      <span className="loc"> {suburb} </span>
+    </div>
+  );
 };
 
 export default Weather;
